d885e25b1a4b3ee018cfc21f1e127ed5697471c9
hvanwelbergen <hvanwelbergen@patty.TechFak.Uni-Bielefeld.DE>
2011-11-25 15:47:50 +0100

    implemented chunking

---
+++ AsapRealizer/src/asap/scheduler/BMLBandTSchedulingHandler.java
@@ -0,0 +1,104 @@
+package asap.scheduler;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import asap.bmlb.*;
+
+
+import hmi.bml.core.BehaviourBlock;
+import hmi.bml.ext.bmlt.BMLTBMLBehaviorAttributes;
+import hmi.elckerlyc.BMLBlockPeg;
+import hmi.elckerlyc.planunit.TimedPlanUnitState;
+import hmi.elckerlyc.scheduler.BMLScheduler;
+import hmi.elckerlyc.scheduler.SchedulingHandler;
+import hmi.elckerlyc.scheduler.SchedulingStrategy;
+
+
+public class BMLBandTSchedulingHandler implements SchedulingHandler
+{
+private final SchedulingStrategy strategy;
+    
+    private final Logger logger = LoggerFactory.getLogger(BMLBandTSchedulingHandler.class.getName());
+    
+    public BMLBandTSchedulingHandler(SchedulingStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+    
+    @Override
+    public void schedule(BehaviourBlock bb, BMLScheduler scheduler)
+    {
+        BMLBBMLBehaviorAttributes bmlbAttr = bb.getBMLBehaviorAttributeExtension(BMLBBMLBehaviorAttributes.class);
+        BMLTBMLBehaviorAttributes bmltAttr = bb.getBMLBehaviorAttributeExtension(BMLTBMLBehaviorAttributes.class);
+        
+        Set<String> appendAfter = new HashSet<String>();
+        Set<String> chunkAfter = new HashSet<String>();
+        for (String bmlId : bmltAttr.getInterruptList())
+        {
+            logger.debug("interrupting {}", bmlId);
+            scheduler.interruptBlock(bmlId);
+        }
+        switch (BMLBSchedulingMechanism.parse(bb.getSchedulingMechanism().getNameStart()))
+        {
+        case REPLACE:
+            scheduler.reset();
+            break;
+        case MERGE:
+            break;
+        case APPEND:
+            appendAfter.addAll(scheduler.getBMLBlocks());
+            break;
+        case CHUNK_AFTER:
+            chunkAfter.addAll(bmlbAttr.getChunkAfterList());
+            break;
+        case APPEND_AFTER:
+            appendAfter.addAll(bmltAttr.getAppendList());
+            appendAfter.retainAll(scheduler.getBMLBlocks());
+            break;
+        }
+        
+        double predictedStart = Math.max(scheduler.predictEndTime(appendAfter),scheduler.predictSubsidingTime(chunkAfter));        
+        scheduler.planningStart(bb.id, predictedStart);
+
+        // logger.debug("Scheduling started at: {}",schedulingClock.getTime());
+        BMLBBlock bbm = new BMLBBlock(bb.id, scheduler, appendAfter, bmltAttr.getOnStartList(), chunkAfter);
+        BMLBlockPeg bmlBlockPeg = new BMLBlockPeg(bb.id, predictedStart);        
+        scheduler.addBMLBlockPeg(bmlBlockPeg);
+
+        strategy.schedule(bb.getSchedulingMechanism(), bb, bmlBlockPeg, scheduler, scheduler.getSchedulingTime());
+        logger.debug("Scheduling finished at: {}", scheduler.getSchedulingTime());
+        scheduler.removeInvalidBehaviors(bb.id);
+
+        predictedStart = Math.max(scheduler.predictEndTime(appendAfter),scheduler.predictSubsidingTime(chunkAfter));
+        scheduler.addBMLBlock(bbm);
+        
+
+        scheduler.planningFinished(bb.id, predictedStart, scheduler.predictEndTime(bb.id));
+        if (bmltAttr.isPrePlanned())
+        {
+            logger.debug("Preplanning {}.", bb.id);
+            bbm.setState(TimedPlanUnitState.PENDING);
+        }
+        else
+        {
+            switch (BMLBSchedulingMechanism.parse(bb.getSchedulingMechanism().getNameStart()))
+            {
+            case REPLACE:
+            case MERGE:
+                scheduler.startBlock(bb.id);
+                break;
+            case APPEND_AFTER:
+            case APPEND:
+            case CHUNK_AFTER:
+                bbm.setState(TimedPlanUnitState.LURKING);
+                scheduler.updateBMLBlocks();                
+                break;
+            }
+        }        
+    }
+
+}
---
+++ AsapRealizer/test/src/asap/scheduler/BMLBandTSchedulingHandlerTest.java
@@ -0,0 +1,136 @@
+package asap.scheduler;
+
+import java.util.Set;
+
+import hmi.bml.core.BehaviourBlock;
+import hmi.bml.ext.bmlt.BMLTBMLBehaviorAttributes;
+import hmi.elckerlyc.BMLBlockPeg;
+import hmi.elckerlyc.planunit.TimedPlanUnitState;
+import hmi.elckerlyc.scheduler.BMLBlock;
+import hmi.elckerlyc.scheduler.BMLScheduler;
+import hmi.elckerlyc.scheduler.SmartBodySchedulingStrategy;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.common.collect.ImmutableSet;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import asap.bmlb.BMLBBMLBehaviorAttributes;
+
+
+/**
+ * Unit test cases for the BMLBandTSchedulingHandler
+ * @author hvanwelbergen
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({BMLScheduler.class})
+public class BMLBandTSchedulingHandlerTest
+{
+    private BMLScheduler mockScheduler = mock(BMLScheduler.class);
+    private BMLBandTSchedulingHandler handler = new BMLBandTSchedulingHandler(new SmartBodySchedulingStrategy());
+    
+    @Before
+    public void setup()
+    {
+        when(mockScheduler.getSchedulingTime()).thenReturn(0d);
+    }
+    
+    private BehaviourBlock createBehaviourBlock(String id)
+    {
+        return createBehaviourBlock(id,"");
+    }
+    
+    private BehaviourBlock createBehaviourBlock(String id, String content)
+    {
+        BehaviourBlock bb = new BehaviourBlock(new BMLBBMLBehaviorAttributes(),new BMLTBMLBehaviorAttributes());
+        bb.readXML("<bml id=\""+id+"\""+ content+"/>");
+        return bb;
+    }
+    
+    private void assertBlockPegAdded(String id, double time)
+    {
+        ArgumentCaptor<BMLBlockPeg> blockPegArgument = ArgumentCaptor.forClass(BMLBlockPeg.class);
+        verify(mockScheduler,times(1)).addBMLBlockPeg(blockPegArgument.capture());
+        assertEquals(id,blockPegArgument.getValue().getId());
+        assertEquals(time,blockPegArgument.getValue().getValue(),0.001);        
+    }
+    
+    private BMLBlock captureBMLBlock()
+    {
+        ArgumentCaptor<BMLBlock> bmlBlockArgument = ArgumentCaptor.forClass(BMLBlock.class);
+        verify(mockScheduler,times(1)).addBMLBlock(bmlBlockArgument.capture());
+        return bmlBlockArgument.getValue();    
+    }
+    
+    @Test
+    public void testMergeBlock()
+    {
+        BehaviourBlock bb = createBehaviourBlock("bml1");         
+        handler.schedule(bb, mockScheduler);
+        
+        BMLBlock block = captureBMLBlock();
+        assertEquals("bml1",block.getBMLId());        
+        assertThat(block,instanceOf(BMLBBlock.class));
+        assertBlockPegAdded("bml1",0d);
+        verify(mockScheduler,times(1)).startBlock("bml1");
+    }
+    
+    @Test
+    @SuppressWarnings("unchecked")    
+    public void testAppendAfter()
+    {
+        BehaviourBlock bb = createBehaviourBlock("bml1","scheduling=\"append-after(bml2,bml3)\"");
+        when(mockScheduler.getBMLBlocks()).thenReturn(ImmutableSet.of("bml2","bml3"));
+        when(mockScheduler.predictEndTime((Set<String>)any())).thenReturn(3d);
+        handler.schedule(bb, mockScheduler);       
+        
+        
+        BMLBlock block = captureBMLBlock();
+        assertEquals("bml1",block.getBMLId());        
+        assertThat(block,instanceOf(BMLBBlock.class));
+        assertEquals(TimedPlanUnitState.LURKING,block.getState());
+        assertBlockPegAdded("bml1",3d);
+        verify(mockScheduler,times(1)).planningStart("bml1", 3d);
+        verify(mockScheduler,times(1)).planningFinished(eq("bml1"), eq(3d),anyDouble());
+    }
+    
+    @Test
+    public void testAppendAfterFinishedBlocks()
+    {
+        BehaviourBlock bb = createBehaviourBlock("bml1","scheduling=\"append-after(bml2,bml3)\"");
+        handler.schedule(bb, mockScheduler);        
+        
+        BMLBlock block = captureBMLBlock();
+        assertEquals("bml1",block.getBMLId());        
+        assertThat(block,instanceOf(BMLBBlock.class));
+        assertEquals(TimedPlanUnitState.LURKING,block.getState());
+        assertBlockPegAdded("bml1",0d);
+        verify(mockScheduler,times(1)).planningStart("bml1", 0d);
+        verify(mockScheduler,times(1)).planningFinished(eq("bml1"), eq(0d),anyDouble());        
+    }
+    
+    @Test
+    @SuppressWarnings("unchecked")    
+    public void testChunkAfter()
+    {
+        BehaviourBlock bb = createBehaviourBlock("bml1","scheduling=\"chunk-after(bml2,bml3)\"");
+        when(mockScheduler.getBMLBlocks()).thenReturn(ImmutableSet.of("bml2","bml3"));
+        when(mockScheduler.predictSubsidingTime((Set<String>)any())).thenReturn(3d);
+        handler.schedule(bb, mockScheduler);   
+        
+        BMLBlock block = captureBMLBlock();
+        assertEquals("bml1",block.getBMLId());        
+        assertThat(block,instanceOf(BMLBBlock.class));
+        assertEquals(TimedPlanUnitState.LURKING,block.getState());
+        verify(mockScheduler,times(1)).planningStart("bml1", 3d);
+        verify(mockScheduler,times(1)).planningFinished(eq("bml1"), eq(3d),anyDouble());
+    }
+}
