43a6fb4c7adbdf1f2b4ad967ddea599f4b09dd3d
hvanwelbergen <hvanwelbergen@patty.TechFak.Uni-Bielefeld.DE>
2012-03-27 09:39:23 +0200

    test for murml resolve

--- AsapFaceEngine/test/src/asap/faceengine/FacePlannerTest.java
+++ AsapFaceEngine/test/src/asap/faceengine/FacePlannerTest.java
@@ -1,19 +1,24 @@
 package asap.faceengine;
 
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import hmi.bml.core.Behaviour;
 import hmi.bml.core.FaceBehaviour;
+import hmi.bml.parser.Constraint;
 import hmi.elckerlyc.BehaviourPlanningException;
 import hmi.elckerlyc.PlannerTests;
 import hmi.elckerlyc.feedback.FeedbackManager;
 import hmi.elckerlyc.feedback.FeedbackManagerImpl;
 import hmi.elckerlyc.pegboard.BMLBlockPeg;
+import hmi.elckerlyc.pegboard.TimePeg;
 import hmi.elckerlyc.planunit.KeyPosition;
 import hmi.elckerlyc.planunit.PlanManager;
 import hmi.elckerlyc.scheduler.BMLBlockManager;
+import hmi.elckerlyc.scheduler.TimePegAndConstraint;
 import hmi.elckerlyc.util.KeyPositionMocker;
+import hmi.elckerlyc.util.TimePegUtil;
 import hmi.faceanimation.FaceController;
 import hmi.faceanimation.converters.EmotionConverter;
 import hmi.faceanimation.converters.FACSConverter;
@@ -29,9 +34,13 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import asap.ext.murml.MURMLFaceBehaviour;
 import asap.faceengine.facebinding.FaceBinding;
 import asap.faceengine.faceunit.FaceUnit;
+import asap.faceengine.faceunit.KeyframeMorphFU;
 import asap.faceengine.faceunit.TimedFaceUnit;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Unit testcases for the FacePlanner
@@ -63,8 +72,7 @@ public class FacePlannerTest
         TimedFaceUnit tmu = new TimedFaceUnit(fbManager, bbPeg, BMLID, "nod1", mockUnit);
         KeyPositionMocker.stubKeyPositions(mockUnit, new KeyPosition("start", 0, 1), new KeyPosition("end", 1, 1));
         final List<TimedFaceUnit> tmus = new ArrayList<TimedFaceUnit>();
-        tmus.add(tmu);        
-        //getFaceUnit(fbManager, bbPeg, b, faceController, facsConverter, emotionConverter)
+        tmus.add(tmu);
         when(mockFaceBinding.getFaceUnit((FeedbackManager) any(), (BMLBlockPeg) any(), (Behaviour)any(), 
                 (FaceController)any(),(FACSConverter)any(),(EmotionConverter)any())).thenReturn(tmus);
         when(mockUnit.getPreferedDuration()).thenReturn(3.0);
@@ -92,5 +100,23 @@ public class FacePlannerTest
     public void testResolveStartOffset() throws IOException, BehaviourPlanningException
     {
         plannerTests.testResolveStartOffset(createFaceBehaviour());
+    }
+    
+    @Test
+    public void testResolveMURML() throws IOException, BehaviourPlanningException
+    {
+        String bmlString = "<murml:murmlface xmlns:murml=\"http://www.techfak.uni-bielefeld.de/ags/soa/murml\" " +
+                "id=\"a1\" start=\"nod1:end\">" +
+                "<definition><keyframing><phase><frame ftime=\"0\"><posture>Humanoid "+
+                "(dB_Smile 3 70 0 0)</posture></frame></phase></keyframing></definition>"+
+                "</murml:murmlface>";
+        MURMLFaceBehaviour b = new MURMLFaceBehaviour(BMLID, new XMLTokenizer(bmlString));
+        ArrayList<TimePegAndConstraint> sacs = new ArrayList<TimePegAndConstraint>();
+        TimePeg startPeg = TimePegUtil.createTimePeg(0);        
+        sacs.add(new TimePegAndConstraint("start",startPeg , new Constraint(), 0, false));
+        
+        TimedFaceUnit tfu = facePlanner.resolveSynchs(bbPeg, b, sacs);
+        assertNotNull(tfu);
+        assertThat(tfu.getFaceUnit(), instanceOf(KeyframeMorphFU.class));        
     }
 }
