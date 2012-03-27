bcde46372f82bbfe7c6f82052ebe4186a3ef88e0
hvanwelbergen <hvanwelbergen@patty.TechFak.Uni-Bielefeld.DE>
2012-03-27 14:40:41 +0200

    testcases for murml description extension

---
+++ AsapBML/test/src/asap/bmlb/parser/BMLBParserTest.java
@@ -0,0 +1,59 @@
+package asap.bmlb.parser;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import hmi.bml.BMLInfo;
+import hmi.bml.core.Behaviour;
+import hmi.bml.core.BehaviourBlock;
+import hmi.bml.parser.BMLParser;
+import hmi.util.Resources;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asap.bmlb.BMLBBMLBehaviorAttributes;
+import asap.ext.murml.MURMLFaceBehaviour;
+
+/**
+ * Unit test cases for a BML parser that can parse BMLB
+ * @author hvanwelbergen
+ *
+ */
+public class BMLBParserTest
+{
+    private static final int PARSE_TIMEOUT = 300;
+    private BMLParser parser;
+    private Resources res = new Resources("bmltest");    
+    private BehaviourBlock block;
+    //private BMLBBMLBehaviorAttributes bmltExt;
+    
+    @Before
+    public void setup()
+    {
+        BMLInfo.addDescriptionExtension(MURMLFaceBehaviour.xmlTag(), MURMLFaceBehaviour.class);
+        BMLInfo.supportedExtensions.add(MURMLFaceBehaviour.class);        
+        parser = new BMLParser();
+    }
+    
+    private void readXML(String file) throws IOException
+    {
+        BMLInfo.supportedExtensions.add(MURMLFaceBehaviour.class);
+        parser.clear();
+        block = new BehaviourBlock(new BMLBBMLBehaviorAttributes());        
+        block.readXML(res.getReader(file));
+        //bmltExt = block.getBMLBehaviorAttributeExtension(BMLBBMLBehaviorAttributes.class);
+        parser.addBehaviourBlock(block);
+    }
+    
+    @Test(timeout = PARSE_TIMEOUT)
+    public void testMurmlFaceDesc()throws IOException
+    {
+        readXML("runtime/murml/murmlfacekeyframedescription.xml");
+        Behaviour b = block.behaviours.get(0);
+        assertEquals("face1", b.id);
+        assertThat(b, instanceOf(MURMLFaceBehaviour.class));
+    }
+}
--- AsapBML/test/src/asap/ext/murml/MURMLFaceBehaviourTest.java
+++ AsapBML/test/src/asap/ext/murml/MURMLFaceBehaviourTest.java
@@ -2,32 +2,52 @@ package asap.ext.murml;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import hmi.bml.BMLInfo;
+import hmi.bml.core.FaceBehaviour;
 import hmi.xml.XMLTokenizer;
 
 import java.io.IOException;
 
 import org.junit.Test;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 /**
  * Unit test cases for MURMLFaceBehaviour
  * @author hvanwelbergen
- *
+ * 
  */
 public class MURMLFaceBehaviourTest
 {
     @Test
     public void testReadKeyframe() throws IOException
     {
-        String bmlString = "<murml:murmlface xmlns:murml=\"http://www.techfak.uni-bielefeld.de/ags/soa/murml\" " +
-        		"id=\"a1\" start=\"nod1:end\">" +
-                "<definition><keyframing><phase><frame ftime=\"0\"><posture>Humanoid "+
-                "(dB_Smile 3 70 0 0)</posture></frame></phase></keyframing></definition>"+
-        		"</murml:murmlface>";
+        String bmlString = "<murml:murmlface xmlns:murml=\"http://www.techfak.uni-bielefeld.de/ags/soa/murml\" "
+                + "id=\"a1\" start=\"nod1:end\">" + "<definition><keyframing><phase><frame ftime=\"0\"><posture>Humanoid "
+                + "(dB_Smile 3 70 0 0)</posture></frame></phase></keyframing></definition>" + "</murml:murmlface>";
         MURMLFaceBehaviour beh = new MURMLFaceBehaviour("bmla", new XMLTokenizer(bmlString));
-        assertEquals("bmla",beh.getBmlId());
-        assertEquals("a1",beh.id);
-        
+        assertEquals("bmla", beh.getBmlId());
+        assertEquals("a1", beh.id);
+
         assertNull(beh.getMurmlDefinition().getPosture());
-        assertEquals(0, beh.getMurmlDefinition().getKeyframing().getPhases().get(0).getFrames().get(0).getFtime(), 0.001);        
+        assertEquals(0, beh.getMurmlDefinition().getKeyframing().getPhases().get(0).getFrames().get(0).getFtime(), 0.001);
+    }
+
+    @Test
+    public void testExtension() throws IOException
+    {
+        BMLInfo.addDescriptionExtension(MURMLFaceBehaviour.xmlTag(), MURMLFaceBehaviour.class);
+        BMLInfo.supportedExtensions.add(MURMLFaceBehaviour.class);
+        String murmlString = "<murml:murmlface xmlns:murml=\"http://www.techfak.uni-bielefeld.de/ags/soa/murml\" "
+                + "id=\"a1\">" + "<definition><keyframing><phase><frame ftime=\"0\"><posture>Humanoid "
+                + "(dB_Smile 3 70 0 0)</posture></frame></phase></keyframing></definition>" + "</murml:murmlface>";
+        String bmlString = "<face id=\"a1\" type=\"BLINK\"><description priority=\"1\" type=\"murmlface\">"+murmlString+"</description></face>";
+        FaceBehaviour f = new FaceBehaviour("bmla",new XMLTokenizer(bmlString));
+        assertThat(f.descBehaviour, instanceOf(MURMLFaceBehaviour.class));
+        MURMLFaceBehaviour beh = (MURMLFaceBehaviour)f.descBehaviour;
+        assertEquals("bmla", beh.getBmlId());
+        assertEquals("a1", beh.id);
+
+        assertNull(beh.getMurmlDefinition().getPosture());
+        assertEquals(0, beh.getMurmlDefinition().getKeyframing().getPhases().get(0).getFrames().get(0).getFtime(), 0.001);
     }
 }
