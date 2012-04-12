8247a4acdd663a77903101c83702fe155b76be05
hvanwelbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2012-04-12 22:42:58 +0200

    checkstyle fixes

--- AsapRealizerIntegrationTests/test/src/asap/realizerintegrationtest/EnvironmentLoadingTest.java
+++ AsapRealizerIntegrationTests/test/src/asap/realizerintegrationtest/EnvironmentLoadingTest.java
@@ -1,17 +1,18 @@
 package asap.realizerintegrationtest;
 
-import java.io.IOException;
-
-import org.junit.Test;
-
+import static org.mockito.Mockito.mock;
 import hmi.bml.parser.BMLParser;
 import hmi.elckerlyc.feedback.NullFeedbackManager;
 import hmi.elckerlyc.pegboard.PegBoard;
 import hmi.elckerlyc.scheduler.BMLBlockManager;
 import hmi.xml.XMLTokenizer;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
 import asap.environment.BMLSchedulerAssembler;
 import asap.utils.SchedulingClock;
-import static org.mockito.Mockito.*;
 
 /**
  * Test cases to load some Asap-specific instantiations of an environment
@@ -22,16 +23,14 @@ public class EnvironmentLoadingTest
     private BMLBlockManager bbm = new BMLBlockManager();
     private PegBoard pegBoard = new PegBoard();
     private SchedulingClock mockClock = mock(SchedulingClock.class);
-    
+
     @Test
     public void testLoadScheduler() throws IOException
     {
-        BMLSchedulerAssembler asm = new BMLSchedulerAssembler("id1", new BMLParser(), NullFeedbackManager.getInstance()
-                ,bbm , mockClock,pegBoard);
-        String str = "<BMLScheduler>"+
-                     "<SchedulingHandler class=\"asap.scheduler.BMLBandTSchedulingHandler\" " +
-                     		"schedulingStrategy=\"hmi.elckerlyc.scheduler.SortedSmartBodySchedulingStrategy\"/>"+
-                     "</BMLScheduler>";
+        BMLSchedulerAssembler asm = new BMLSchedulerAssembler("id1", new BMLParser(), NullFeedbackManager.getInstance(), bbm, mockClock,
+                pegBoard);
+        String str = "<BMLScheduler>" + "<SchedulingHandler class=\"asap.scheduler.BMLBandTSchedulingHandler\" "
+                + "schedulingStrategy=\"hmi.elckerlyc.scheduler.SortedSmartBodySchedulingStrategy\"/>" + "</BMLScheduler>";
         asm.readXML(new XMLTokenizer(str));
     }
 }
