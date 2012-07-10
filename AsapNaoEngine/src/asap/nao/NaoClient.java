d4425ebe748a64221d69389cad29c244dabc2cad
herwin <welberge@ewi.utwente.nl>
2012-07-10 17:40:26 +0200

    asapnao project added

---
+++ AsapNaoEngine/src/asap/nao/NaoClient.java
@@ -0,0 +1,25 @@
+package asap.nao;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class NaoClient {
+	public static void main(String argv[]) throws Exception {
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
+		Socket clientSocket = new Socket("localhost", 8881);
+		DataOutputStream outToServer = new DataOutputStream(
+				clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+	}
+}
---
+++ AsapNaoEngine/src/asap/nao/naobinding/NBParameterDefaults.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (C) 2009 Human Media Interaction, University of Twente, the Netherlands
+ * 
+ * This file is part of the Elckerlyc BML realizer.
+ * 
+ * Elckerlyc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Elckerlyc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Elckerlyc.  If not, see http://www.gnu.org/licenses/.
+ ******************************************************************************/
+package asap.nao.naobinding;
+
+import hmi.xml.XMLStructureAdapter;
+import hmi.xml.XMLTokenizer;
+
+import java.io.IOException;
+
+/**
+ * Holds default parameters values for a NaoUnit.
+ * @author Robin ten Buuren
+ *
+ */
+public class NBParameterDefaults extends XMLStructureAdapter
+{
+    private NaoUnitSpec spec;
+    
+    public NBParameterDefaults(NaoUnitSpec s)
+    {
+        spec = s;
+    }
+    
+    @Override
+    public void decodeContent(XMLTokenizer tokenizer) throws IOException
+    {
+        while (tokenizer.atSTag())
+        {
+            String tag = tokenizer.getTagName();
+            if (tag.equals(NaoUnitParameterDefault.xmlTag())) 
+            {
+                NaoUnitParameterDefault nupc = new NaoUnitParameterDefault();
+                nupc.readXML(tokenizer);
+                spec.addParameterDefault(nupc);
+            }
+        }
+    }
+    
+   /*
+    * The XML Stag for XML encoding
+    */
+   private static final String XMLTAG = "parameterdefaults";
+ 
+   /**
+    * The XML Stag for XML encoding -- use this static method when you want to see if a given String equals
+    * the xml tag for this class
+    */
+   public static String xmlTag() { return XMLTAG; }
+ 
+   /**
+    * The XML Stag for XML encoding -- use this method to find out the run-time xml tag of an object
+    */
+   @Override
+   public String getXMLTag() {
+      return XMLTAG;
+   }
+
+}
