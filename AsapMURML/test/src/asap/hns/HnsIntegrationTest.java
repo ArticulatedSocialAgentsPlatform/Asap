ca66d6c11a1cd1672c22a683e64cd4bfe8e84683
Herwin van Welbergen <hvanwelbergen@patty.TechFak.Uni-Bielefeld.DE>
2012-08-08 16:07:19 +0200

    more MURML stuff

--- AsapMURML/test/src/asap/hns/HnsIntegrationTest.java
+++ AsapMURML/test/src/asap/hns/HnsIntegrationTest.java
@@ -1,5 +1,7 @@
 package asap.hns;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import hmi.util.Resources;
@@ -18,5 +20,6 @@ public class HnsIntegrationTest
     {
         Hns hns = new Hns();
         hns.readXML(new Resources("").getReader("billie_hns.xml"));
+        assertEquals("-90.0", hns.getSymbolValue("palmOrientations", "PalmU"));
     }
 }
