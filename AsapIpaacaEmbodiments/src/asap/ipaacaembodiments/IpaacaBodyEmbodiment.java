4316a5934013e28f19868bf70fd4d9d75540c727
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2012-09-24 13:08:41 +0200

    fixed name in ivy

--- AsapIpaacaEmbodiments/src/asap/ipaacaembodiments/IpaacaBodyEmbodiment.java
+++ AsapIpaacaEmbodiments/src/asap/ipaacaembodiments/IpaacaBodyEmbodiment.java
@@ -1,15 +1,36 @@
 package asap.ipaacaembodiments;
 
+import hmi.animation.VJoint;
 import hmi.environmentbase.CopyEmbodiment;
 
+/**
+ * Sends joint rotations from its animation joint to a renderer through Ipaaca. 
+ * Assumes that the animation joint is not changed during the copy(). That is: assumes that there is only one thread accessing animationJoint.
+ * @author hvanwelbergen
+ *
+ */
 public class IpaacaBodyEmbodiment implements CopyEmbodiment
 {
-    private String id;
+    private final String id;
+    private final VJoint animationJoint;    
+    private IpaacaEmbodiment ipaacaEmbodiment;
+    
+    public IpaacaBodyEmbodiment(String id, VJoint animationJoint, IpaacaEmbodiment ipaacaEmbodiment)
+    {
+        this.id = id;
+        this.animationJoint = animationJoint;      
+        this.ipaacaEmbodiment = ipaacaEmbodiment;
+    }
+    
+    public void init()
+    {
+        ipaacaEmbodiment.getAvailableJoints();
+    }
     
     @Override
     public void copy()
     {
-        // TODO Auto-generated method stub
+        animationJoint.calculateMatrices();   
         
     }
 
