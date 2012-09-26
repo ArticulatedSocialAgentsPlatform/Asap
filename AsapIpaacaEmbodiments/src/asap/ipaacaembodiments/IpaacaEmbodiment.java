3cd93c81e88b159480a750a6155b6db5aa4aa8c2
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2012-09-26 18:06:43 +0200

    coord transform fix

--- AsapIpaacaEmbodiments/src/asap/ipaacaembodiments/IpaacaEmbodiment.java
+++ AsapIpaacaEmbodiments/src/asap/ipaacaembodiments/IpaacaEmbodiment.java
@@ -2,6 +2,7 @@ package asap.ipaacaembodiments;
 
 import hmi.animation.VJoint;
 import hmi.environmentbase.Embodiment;
+import hmi.math.Mat3f;
 import hmi.math.Mat4f;
 import hmi.math.Quat4f;
 import hmi.math.Vec3f;
@@ -41,12 +42,12 @@ public class IpaacaEmbodiment implements Embodiment
     private AtomicReference<List<String>> availableJoints = new AtomicReference<>();
     private List<String> usedMorphs = new ArrayList<>();
     private List<String> usedJoints = new ArrayList<>();
-    
+
     static
     {
         Initializer.initializeIpaacaRsb();
     }
-    
+
     public void setId(String id)
     {
         this.id = id;
@@ -74,68 +75,97 @@ public class IpaacaEmbodiment implements Embodiment
         outBuffer = new OutputBuffer("ipaacaenvironment" + id);
     }
 
-    private String getMatrix(float []m)
+    private String getMatrix(float[] m)
     {
         StringBuffer buf = new StringBuffer();
-        for(float f:m)
+        for (float f : m)
         {
             buf.append(f);
             buf.append(" ");
         }
         return buf.toString().trim();
     }
-    
+
+    private float[] coordinateRelocate(float m[])
+    {
+        // x <= z
+        // y <= x
+        // z <= y
+        float res[] = Mat4f.getMat4f();
+        Mat4f.setIdentity(res);
+        float t[] = Vec3f.getVec3f();
+        Mat4f.getTranslation(t, m);
+        float x = t[0], y = t[1], z = t[2];
+        t[0] = z;
+        t[1] = x;
+        t[2] = y;
+        float q[] = Quat4f.getQuat4f();
+        Quat4f.setFromMat4f(q, m);
+        x = q[Quat4f.x];
+        y = q[Quat4f.y];
+        z = q[Quat4f.z];
+        q[Quat4f.x] = z;
+        q[Quat4f.y] = x;
+        q[Quat4f.z] = y;
+        Mat4f.setRotation(res, q);
+        Mat4f.setTranslation(res, t);
+        return res;
+    }
+
     private String getJointMatrices(List<VJoint> jointList)
     {
         StringBuffer buf = new StringBuffer();
-        for(VJoint vj:jointList)
+        for (VJoint vj : jointList)
         {
-            buf.append(getMatrix(vj.getLocalMatrix()));
+
+            buf.append(getMatrix(coordinateRelocate(vj.getLocalMatrix())));
             buf.append(" ");
-            float m[]=vj.getGlobalMatrix();
-            float mRes[]=new float[16];
-            float q[]=Quat4f.getQuat4f();
+
+            float m[] = new float[16];
+            Mat4f.set(m, vj.getGlobalMatrix());
+            float mRes[] = new float[16];
+            float q[] = Quat4f.getQuat4f();
             vj.getRotation(q);
-            float mQ[]=new float[16];
+            float mQ[] = new float[16];
             Mat4f.setFromTR(mQ, Vec3f.getZero(), q);
             Mat4f.invertRigid(mQ);
-            Mat4f.mul(mRes, m,mQ);
-            buf.append(getMatrix(mRes));
+            Mat4f.mul(mRes, m, mQ);
+            buf.append(getMatrix(coordinateRelocate(mRes)));
             buf.append(" ");
         }
         return buf.toString().trim();
     }
-    
+
     public void setJointData(List<VJoint> jointList, ImmutableMap<String, Float> morphTargets)
     {
         List<String> usedTargets = new ArrayList<>();
         List<String> values = new ArrayList<>();
-        for(String morph:availableMorphs.get())
+        for (String morph : availableMorphs.get())
         {
-            if(morphTargets.keySet().contains(morph))
+            if (morphTargets.keySet().contains(morph))
             {
                 usedTargets.add(morph);
-                values.add(""+morphTargets.get(morph)*100);
+                values.add("" + morphTargets.get(morph) * 100);
             }
         }
-        
-        LocalMessageIU iu = new LocalMessageIU();     
+
+        LocalMessageIU iu = new LocalMessageIU();
         iu.setCategory("jointData");
         iu.getPayload().put("morph_data", toSpaceSeperatedList(values));
-        if(jointList.isEmpty())
+        if (jointList.isEmpty())
         {
-            //XXX no jointlist: add dummy joint data to force re-render of face
-            setUsed(ImmutableList.of(availableJoints.get().get(0)),usedTargets);
+            // XXX no jointlist: add dummy joint data to force re-render of face
+            setUsed(ImmutableList.of(availableJoints.get().get(0)), usedTargets);
             iu.getPayload().put("joint_data", "1 0 0 0  0 1 0 0  0 0 1 0  0 0 0 1    1 0 0 0  0 1 0 0  0 0 1 0  0 0 0 1");
         }
         else
         {
-            setUsed(usedJoints,usedTargets);
+            setUsed(usedJoints, usedTargets);
             iu.getPayload().put("joint_data", getJointMatrices(jointList));
         }
         outBuffer.add(iu);
     }
-    
+
     public ImmutableSet<String> getAvailableJoints()
     {
         return ImmutableSet.copyOf(availableJoints.get());
@@ -148,20 +178,20 @@ public class IpaacaEmbodiment implements Embodiment
 
     private String toCommaSeperatedList(List<String> strSet)
     {
-        return toSeperatedList(strSet,",");
+        return toSeperatedList(strSet, ",");
     }
-    
+
     private String toSpaceSeperatedList(List<String> strSet)
     {
-        return toSeperatedList(strSet," ");
+        return toSeperatedList(strSet, " ");
     }
-    
+
     private String toSeperatedList(List<String> strSet, String seperator)
     {
         StringBuffer sBuf = new StringBuffer();
-        for (String s:strSet)
+        for (String s : strSet)
         {
-            if(sBuf.length()>0)
+            if (sBuf.length() > 0)
             {
                 sBuf.append(seperator);
             }
@@ -169,22 +199,22 @@ public class IpaacaEmbodiment implements Embodiment
         }
         return sBuf.toString();
     }
-    
+
     private void updateUsedSet()
     {
         LocalMessageIU iuConfig = new LocalMessageIU();
         iuConfig.setCategory("jointDataConfigReply");
-        iuConfig.getPayload().put("joints_provided",toCommaSeperatedList(usedJoints));
+        iuConfig.getPayload().put("joints_provided", toCommaSeperatedList(usedJoints));
         List<String> np = new ArrayList<>(availableJoints.get());
         np.removeAll(usedJoints);
-        iuConfig.getPayload().put("joints_not_provided",toCommaSeperatedList(np));
-        iuConfig.getPayload().put("morphs_provided",toCommaSeperatedList(usedMorphs));
+        iuConfig.getPayload().put("joints_not_provided", toCommaSeperatedList(np));
+        iuConfig.getPayload().put("morphs_provided", toCommaSeperatedList(usedMorphs));
         np = new ArrayList<>(availableMorphs.get());
         np.removeAll(usedMorphs);
-        iuConfig.getPayload().put("morphs_not_provided",toCommaSeperatedList(np));
+        iuConfig.getPayload().put("morphs_not_provided", toCommaSeperatedList(np));
         outBuffer.add(iuConfig);
     }
-    
+
     public void setUsed(List<String> usedJoints, List<String> usedMorphs)
     {
         if (!usedJoints.equals(this.usedJoints) || !usedMorphs.equals(this.usedMorphs))
@@ -194,7 +224,7 @@ public class IpaacaEmbodiment implements Embodiment
             updateUsedSet();
         }
     }
-    
+
     public void setUsedJoints(List<String> usedJoints)
     {
         if (!usedJoints.equals(this.usedJoints))
@@ -206,7 +236,7 @@ public class IpaacaEmbodiment implements Embodiment
 
     public void setUsedMorphs(List<String> usedMorphs)
     {
-        if(!usedMorphs.equals(this.usedMorphs))
+        if (!usedMorphs.equals(this.usedMorphs))
         {
             this.usedMorphs = ImmutableList.copyOf(usedMorphs);
             updateUsedSet();
@@ -231,7 +261,7 @@ public class IpaacaEmbodiment implements Embodiment
             {
                 outBuffer.close();
             }
-        }        
+        }
     }
 
     private void setAvailableJoints(String[] joints)
