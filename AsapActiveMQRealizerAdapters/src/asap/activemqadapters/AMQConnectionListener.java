4333da4c43c71b1c881f27088859ed93a266a6b0
hvanwelbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2012-10-28 23:19:46 +0100

    ActiveMQAdapters

---
+++ AsapActiveMQRealizerAdapters/src/asap/activemqadapters/AMQConnectionListener.java
@@ -0,0 +1,13 @@
+package asap.activemqadapters;
+
+import javax.jms.TextMessage;
+
+/**
+ * Interface for objects that want to listen to messages received by an AMQConnection
+ * @author dennisr
+ *
+ */
+public interface AMQConnectionListener {
+	void onMessage(TextMessage m);
+    
+}
---
+++ AsapActiveMQRealizerAdapters/src/asap/activemqadapters/loader/ActiveMQToBMLRealizerAdapterLoader.java
@@ -0,0 +1,48 @@
+package asap.activemqadapters.loader;
+
+import java.io.IOException;
+
+import javax.jms.JMSException;
+
+import lombok.extern.slf4j.Slf4j;
+
+import hmi.util.Clock;
+import hmi.xml.XMLScanException;
+import hmi.xml.XMLTokenizer;
+import asap.activemqadapters.ActiveMQToBMLRealizerAdapter;
+import asap.bml.bridge.RealizerPort;
+import asap.realizerembodiments.PipeLoader;
+
+/**
+ * Loader for and ActiveMQToRealizerAdapter
+ * @author Dennis Reidsma
+ * @author Herwin
+ */
+@Slf4j
+public class ActiveMQToBMLRealizerAdapterLoader implements PipeLoader
+{
+    private RealizerPort adaptedRealizerPort = null;
+
+    @Override
+    public void readXML(XMLTokenizer theTokenizer, String id, String vhId, String name, RealizerPort realizerPort, Clock theSchedulingClock)
+            throws XMLScanException, IOException
+    {
+        try
+        {
+            new ActiveMQToBMLRealizerAdapter(realizerPort);
+        }
+        catch (JMSException e)
+        {
+            log.error("Error registering at the ActiveMQ network: {}", e);
+            throw new XMLScanException("Error registering at the ActiveMQ network");
+        }
+        adaptedRealizerPort = realizerPort;
+        if (!theTokenizer.atETag("PipeLoader")) throw new XMLScanException("ActiveMQPipeLoader is an empty element");
+    }
+
+    @Override
+    public RealizerPort getAdaptedRealizerPort()
+    {
+        return adaptedRealizerPort;
+    }
+}
