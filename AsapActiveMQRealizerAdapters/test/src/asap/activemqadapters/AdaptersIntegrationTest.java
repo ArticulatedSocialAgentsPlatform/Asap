73e0a7f498afc6d30ad6ad0ec90dbaf2c1e33c98
hvanwelbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2012-10-29 22:26:16 +0100

    re-enabled active mq tests

--- AsapActiveMQRealizerAdapters/test/src/asap/activemqadapters/AdaptersIntegrationTest.java
+++ AsapActiveMQRealizerAdapters/test/src/asap/activemqadapters/AdaptersIntegrationTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.verify;
 import javax.jms.JMSException;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import asap.bml.bridge.RealizerPort;
@@ -31,7 +30,6 @@ public class AdaptersIntegrationTest
         ipaacaToBML = new ActiveMQToBMLRealizerAdapter(mockRealizerPort);
     }
     
-    @Ignore //FIXME: re-enable when an ActiveMQ server is running properly on the daily build server
     @Test(timeout=1000)
     public void testPerformBML() throws InterruptedException
     {
@@ -40,7 +38,6 @@ public class AdaptersIntegrationTest
         verify(mockRealizerPort).performBML("bmltest");
     }
     
-    @Ignore //FIXME: re-enable when an ActiveMQ server is running properly on the daily build server
     @Test(timeout=1000)
     public void testFeedback() throws InterruptedException
     {
--- AsapActiveMQRealizerAdapters/test/src/asap/activemqadapters/loader/ActiveMQToBMLRealizerAdapterLoaderTest.java
+++ AsapActiveMQRealizerAdapters/test/src/asap/activemqadapters/loader/ActiveMQToBMLRealizerAdapterLoaderTest.java
@@ -10,7 +10,6 @@ import hmi.xml.XMLTokenizer;
 
 import java.io.IOException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import asap.bml.bridge.RealizerPort;
@@ -25,7 +24,6 @@ public class ActiveMQToBMLRealizerAdapterLoaderTest
     private RealizerPort mockRealizerPort = mock(RealizerPort.class);
     private Clock mockSchedulingClock = mock(Clock.class);
     
-    @Ignore //FIXME: re-enable when an ActiveMQ server is running properly on the daily build server
     @Test
     public void testReadFromXML() throws IOException
     {
