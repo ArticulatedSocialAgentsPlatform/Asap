d6016561aaac3ba9e0bd1523a2b60e04c52c24ca
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-01-10 16:34:08 +0100

    Sapi binding

---
+++ AsapSapi5TTSBinding/src/asap/sapittsbinding/SAPITTSBindingFactory.java
@@ -0,0 +1,19 @@
+package asap.sapittsbinding;
+
+import asap.speechengine.ttsbinding.TTSBinding;
+import asap.speechengine.ttsbinding.TTSBindingFactory;
+
+
+/**
+ * Produces a SAPITTSBinding 
+ * @author hvanwelbergen
+ *
+ */
+public class SAPITTSBindingFactory implements TTSBindingFactory
+{
+    @Override
+    public TTSBinding createBinding()
+    {
+        return new SAPITTSBinding();
+    }
+}
