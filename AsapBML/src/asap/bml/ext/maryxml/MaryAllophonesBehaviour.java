65890c732d480c9d0adcacb01ed4ba769ee6d2c3
hvanwelbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2013-01-12 23:06:10 +0100

    got rid of some code duplication.

--- AsapBML/src/asap/bml/ext/maryxml/MaryAllophonesBehaviour.java
+++ AsapBML/src/asap/bml/ext/maryxml/MaryAllophonesBehaviour.java
@@ -1,68 +1,16 @@
 package asap.bml.ext.maryxml;
 
-import saiba.bml.core.SpeechBehaviour;
-import hmi.xml.XMLFormatting;
 import hmi.xml.XMLTokenizer;
 
 import java.io.IOException;
-import java.util.HashMap;
 /**
  * MaryAllophones Behavior 
  * @author reidsma
  */
-public class MaryAllophonesBehaviour extends SpeechBehaviour
+public class MaryAllophonesBehaviour extends MaryXMLBaseBehaviour
 {
     public MaryAllophonesBehaviour(String bmlId, XMLTokenizer tokenizer) throws IOException
     {
         super(bmlId, tokenizer);        
     }
-    
-    @Override
-    public void decodeAttributes(HashMap<String, String> attrMap,
-            XMLTokenizer tokenizer)
-    {
-        // empty, 'cause id is not required
-    }
-
-    @Override
-    public void decodeContent(XMLTokenizer tokenizer) throws IOException
-    {
-        content = tokenizer.getXMLSectionContent();        
-    }
-
-    /*
-     * The XML Stag for XML encoding
-     */
-    private static final String XMLTAG = "maryxml";
-
-    /**
-     * The XML Stag for XML encoding -- use this static method when you want to
-     * see if a given String equals the xml tag for this class
-     */
-    public static String xmlTag()
-    {
-        return XMLTAG;
-    }
-    
-    @Override
-    public StringBuilder appendContent(StringBuilder buf, XMLFormatting fmt)
-    {
-        if (content != null) buf.append(content);
-        return buf;
-    }
-
-    /**
-     * The XML Stag for XML encoding -- use this method to find out the run-time
-     * xml tag of an object
-     */
-    @Override
-    public String getXMLTag()
-    {
-        return XMLTAG;
-    }
-    
-    static final String NAMESPACE = "http://mary.dfki.de/2002/MaryXML";
-    
-    @Override
-    public  String getNamespace() { return NAMESPACE; }
 }
---
+++ AsapBML/test/src/asap/bml/ext/maryxml/MaryXMLBaseBehaviourTest.java
@@ -0,0 +1,81 @@
+package asap.bml.ext.maryxml;
+
+import static org.junit.Assert.assertEquals;
+import hmi.testutil.LabelledParameterized;
+import hmi.xml.XMLTokenizer;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Unit test cases for the MaryXMLBaseBehaviour
+ * @author welberge
+ * 
+ */
+@RunWith(LabelledParameterized.class)
+public class MaryXMLBaseBehaviourTest
+{
+    private MaryXMLBaseBehaviour beh;
+    private MaryXMLBehaviorFactory factory;
+
+    private static final class MaryXMLBehaviorFactory
+    {
+        private final Class<? extends MaryXMLBaseBehaviour> behClass;
+
+        public MaryXMLBehaviorFactory(Class<? extends MaryXMLBaseBehaviour> behClass)
+        {
+            this.behClass = behClass;
+        }
+
+        public MaryXMLBaseBehaviour createBehavior(String bmlId, String bml) throws IOException, InstantiationException,
+                IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException
+        {
+            return behClass.getConstructor(String.class, XMLTokenizer.class).newInstance(bmlId, new XMLTokenizer(bml));
+        }
+    }
+
+    @Parameters
+    public static Collection<Object[]> data()
+    {
+        return Arrays.asList(new Object[] { "MaryXMLBaseBehaviour", new MaryXMLBehaviorFactory(MaryXMLBaseBehaviour.class) }, new Object[] {
+                "MaryXMLBehaviour", new MaryXMLBehaviorFactory(MaryXMLBehaviour.class) }, new Object[] { "MaryAllophonesBehaviour",
+                new MaryXMLBehaviorFactory(MaryAllophonesBehaviour.class) }, new Object[] { "MaryWordsBehaviour",
+                new MaryXMLBehaviorFactory(MaryWordsBehaviour.class) });
+    }
+
+    public MaryXMLBaseBehaviourTest(String label, MaryXMLBehaviorFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    @Before
+    public void setup() throws IOException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException
+    {
+        String str = "<maryxml xmlns=\"" + MaryXMLBaseBehaviour.NAMESPACE + "\" >" + "Hello world!" + "</maryxml>";
+        beh = factory.createBehavior("bml1", str);
+    }
+
+    @Test
+    public void testReadXML() throws IOException
+    {
+        assertEquals("Hello world!", beh.getContent());
+    }
+
+    @Test
+    public void testWriteXML() throws IOException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException
+    {
+        StringBuilder buf = new StringBuilder();
+        beh.appendXML(buf);
+        MaryXMLBaseBehaviour behOut = factory.createBehavior("bml1", buf.toString());
+        assertEquals("Hello world!", behOut.getContent().trim());
+    }
+}
