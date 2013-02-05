5fa4687248456734fb70eabd1e790d45a85be8f8
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-02-05 10:48:21 +0100

    lipsync coarticulation+test cases

--- AsapFaceEngine/src/asap/faceengine/lipsync/TimedFaceUnitIncrementalLipSynchProvider.java
+++ AsapFaceEngine/src/asap/faceengine/lipsync/TimedFaceUnitIncrementalLipSynchProvider.java
@@ -19,44 +19,46 @@ import asap.realizer.planunit.TimedPlanUnitState;
 /**
  * Provides incremental lipsync using TimedFaceUnits.
  * @author hvanwelbergen
- *
+ * 
  */
-public class TimedFaceUnitIncrementalLipSynchProvider implements IncrementalLipSynchProvider 
+public class TimedFaceUnitIncrementalLipSynchProvider implements IncrementalLipSynchProvider
 {
     private final VisemeBinding visimeBinding;
     private final FaceController faceController;
-    private final PlanManager<TimedFaceUnit>facePlanManager;
-    
+    private final PlanManager<TimedFaceUnit> facePlanManager;
+
     private Map<Object, TimedFaceUnit> tfuMap = new HashMap<>();
-    public TimedFaceUnitIncrementalLipSynchProvider(VisemeBinding visBinding, FaceController fc, PlanManager<TimedFaceUnit>facePlanManager)
+    private Map<TimedFaceUnit, Visime> tfuToVisimeMap = new HashMap<>();
+
+    public TimedFaceUnitIncrementalLipSynchProvider(VisemeBinding visBinding, FaceController fc, PlanManager<TimedFaceUnit> facePlanManager)
     {
         visimeBinding = visBinding;
         faceController = fc;
-        this.facePlanManager= facePlanManager; 
+        this.facePlanManager = facePlanManager;
     }
-    
-    private TimedFaceUnit getPrevious(TimedFaceUnit tfuCurr, String bmlId, String id)
+
+    private TimedFaceUnit getPrevious(String bmlId, String id, double startTime)
     {
-        Collection<TimedFaceUnit> tfus = facePlanManager.getPlanUnits(bmlId,id);
+        Collection<TimedFaceUnit> tfus = facePlanManager.getPlanUnits(bmlId, id);
         TimedFaceUnit tfuPrevious = null;
-        for(TimedFaceUnit tfu:tfus)
+        for (TimedFaceUnit tfu : tfus)
         {
-            if(tfu.getStartTime()<tfuCurr.getStartTime())
+            if (tfu.getStartTime() < startTime  && tfu.getStartTime() != TimePeg.VALUE_UNKNOWN)
             {
-                if(tfuPrevious==null || tfu.getStartTime()>tfuPrevious.getStartTime())
+                if (tfuPrevious == null || tfu.getStartTime() > tfuPrevious.getStartTime())
                 {
                     tfuPrevious = tfu;
-                }                
+                }
             }
         }
         return tfuPrevious;
     }
-    
+
     @Override
     public void setLipSyncUnit(BMLBlockPeg bbPeg, Behaviour beh, double start, Visime vis, Object identifier)
     {
         TimedFaceUnit tfu = tfuMap.get(identifier);
-        if(tfu==null)
+        if (tfu == null)
         {
             tfu = visimeBinding.getVisemeUnit(bbPeg, beh, vis.getNumber(), faceController);
             tfu.setTimePeg("start", new TimePeg(bbPeg));
@@ -64,14 +66,22 @@ public class TimedFaceUnitIncrementalLipSynchProvider implements IncrementalLipS
             tfu.setSubUnit(true);
             tfu.setState(TimedPlanUnitState.LURKING);
             facePlanManager.addPlanUnit(tfu);
-            tfuMap.put(identifier, tfu);        
-            
+            tfuMap.put(identifier, tfu);            
         }
+        TimedFaceUnit tfuPrevious = getPrevious(beh.getBmlId(), beh.id, start);
         
-        //TODO: setup crude co-articulation mechanism from TimedFaceUnitLipSynchProvider
-        TimedFaceUnit tfuPrevious = getPrevious(tfu, beh.getBmlId(), beh.id);
-        
-        tfu.getTimePeg("start").setGlobalValue(start);
-        tfu.getTimePeg("end").setGlobalValue(start+(double)vis.getDuration()/1000d);                
+        if (tfuPrevious != null)
+        {
+            Visime prevVis = tfuToVisimeMap.get(tfuPrevious);
+            double prevDuration = (double)prevVis.getDuration()/1000d;
+            tfu.getTimePeg("start").setGlobalValue(start - prevDuration * 0.5);
+            tfuPrevious.getTimePeg("end").setGlobalValue(start + (double)vis.getDuration()/1000d * 0.5);
+        }
+        else
+        {
+            tfu.getTimePeg("start").setGlobalValue(start);
+        }
+        tfuToVisimeMap.put(tfu,vis);
+        tfu.getTimePeg("end").setGlobalValue(start + (double) vis.getDuration() / 1000d);
     }
 }
--- AsapFaceEngine/test/src/asap/faceengine/lipsync/TimedFaceUnitIncrementalLipSynchProviderTest.java
+++ AsapFaceEngine/test/src/asap/faceengine/lipsync/TimedFaceUnitIncrementalLipSynchProviderTest.java
@@ -1,10 +1,7 @@
 package asap.faceengine.lipsync;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.number.OrderingComparison.greaterThan;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import hmi.faceanimation.FaceController;
 import hmi.tts.Visime;
 import hmi.xml.XMLTokenizer;
@@ -21,27 +18,22 @@ import asap.faceengine.viseme.MorphVisemeBinding;
 import asap.faceengine.viseme.VisemeBinding;
 import asap.faceengine.viseme.VisemeToMorphMapping;
 import asap.realizer.pegboard.BMLBlockPeg;
-import asap.realizer.pegboard.TimePeg;
 import asap.realizer.planunit.PlanManager;
-import asap.realizer.planunit.TimedPlanUnit;
-
-import com.google.common.collect.ImmutableList;
 
 /**
- * Unit test cases for TimedFaceUnitLipSyncProvider
- * @author Herwin
- * 
+ * Unit tests for the TimedFaceUnitIncrementalLipSynchProvider
+ * @author hvanwelbergen
+ *
  */
-public class TimedFaceUnitLipSynchProviderTest
+public class TimedFaceUnitIncrementalLipSynchProviderTest
 {
     private PlanManager<TimedFaceUnit> faceManager = new PlanManager<TimedFaceUnit>();
     private VisemeBinding visemeBinding;
     private FaceController mockFaceController = mock(FaceController.class);
-    private TimedPlanUnit mockSpeechUnit = mock(TimedPlanUnit.class);
     private SpeechBehaviour speechBehavior;
     private BMLBlockPeg bbPeg = BMLBlockPeg.GLOBALPEG;
     private static final double TIMING_PRECISION = 0.0001;
-    private TimedFaceUnitLipSynchProvider provider;
+    private TimedFaceUnitIncrementalLipSynchProvider provider; 
     
     @Before
     public void before() throws IOException
@@ -53,37 +45,42 @@ public class TimedFaceUnitLipSynchProviderTest
         visemeBinding = new MorphVisemeBinding(mapping);
         String str = "<speech xmlns=\"http://www.bml-initiative.org/bml/bml-1.0\" " + "id=\"s1\"><text>Hello world</text></speech>";
         speechBehavior = new SpeechBehaviour("bml1", new XMLTokenizer(str));
-
-        TimePeg startPeg = new TimePeg(bbPeg);
-        startPeg.setLocalValue(0);
-        TimePeg endPeg = new TimePeg(bbPeg);
-        endPeg.setLocalValue(10);
-        when(mockSpeechUnit.getTimePeg("start")).thenReturn(startPeg);
-        when(mockSpeechUnit.getTimePeg("end")).thenReturn(endPeg);
-        provider = new TimedFaceUnitLipSynchProvider(visemeBinding, mockFaceController, faceManager);
+        provider = new TimedFaceUnitIncrementalLipSynchProvider(visemeBinding, mockFaceController, faceManager);
     }
-
+    
     @Test
     public void test()
     {
-        provider.addLipSyncMovement(bbPeg, speechBehavior, mockSpeechUnit,
-                ImmutableList.of(new Visime(1, 5000, false), new Visime(2, 5000, false)));
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 0, new Visime(1, 5000, false), new Object());
         List<TimedFaceUnit> faceUnits = faceManager.getPlanUnits();
         assertEquals(0, faceUnits.get(0).getStartTime(), TIMING_PRECISION);
-        assertEquals(10, faceUnits.get(faceUnits.size() - 1).getEndTime(), TIMING_PRECISION);
-
-        for (TimedFaceUnit fu : faceUnits)
-        {
-            assertThat("FaceUnit with 0 duration found at index " + faceUnits.indexOf(fu) + "Face Unit list: " + faceUnits, fu.getEndTime()
-                    - fu.getStartTime(), greaterThan(0d));
-        }
+        assertEquals(5, faceUnits.get(0).getEndTime(), TIMING_PRECISION);
+        assertEquals("bml1",faceUnits.get(0).getBMLId());
+        assertEquals("s1",faceUnits.get(0).getId());
+    }
+    
+    @Test
+    public void testReplace()
+    {
+        Object identifier = new Object();
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 0, new Visime(1, 5000, false), identifier);        
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 0, new Visime(1, 4000, false), identifier);
+        List<TimedFaceUnit> faceUnits = faceManager.getPlanUnits();        
+        assertEquals(1,faceUnits.size());
+        assertEquals(0, faceUnits.get(0).getStartTime(), TIMING_PRECISION);
+        assertEquals(4, faceUnits.get(0).getEndTime(), TIMING_PRECISION);
+        assertEquals("bml1",faceUnits.get(0).getBMLId());
+        assertEquals("s1",faceUnits.get(0).getId());
     }
     
     @Test
     public void testCoArticulation()
     {
-        provider.addLipSyncMovement(bbPeg, speechBehavior, mockSpeechUnit,
-                ImmutableList.of(new Visime(1, 1000, false), new Visime(2, 500, false), new Visime(2, 750, false), new Visime(1, 250, false)));
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 0, new Visime(1, 1000, false), new Object());  
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 1, new Visime(2, 500, false), new Object());
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 1.5, new Visime(2, 750, false), new Object());
+        provider.setLipSyncUnit(bbPeg, speechBehavior, 2.25, new Visime(1, 250, false), new Object());
+        
         List<TimedFaceUnit> faceUnits = faceManager.getPlanUnits();
         assertEquals(0, faceUnits.get(0).getStartTime(), TIMING_PRECISION);
         assertEquals(2.5, faceUnits.get(3).getEndTime(), TIMING_PRECISION);
@@ -93,6 +90,6 @@ public class TimedFaceUnitLipSynchProviderTest
         assertEquals(1.875, faceUnits.get(1).getEndTime(), TIMING_PRECISION);
         assertEquals(1.25, faceUnits.get(2).getStartTime(), TIMING_PRECISION);
         assertEquals(2.375, faceUnits.get(2).getEndTime(), TIMING_PRECISION);
-        assertEquals(1.875, faceUnits.get(3).getStartTime(), TIMING_PRECISION);        
+        assertEquals(1.875, faceUnits.get(3).getStartTime(), TIMING_PRECISION); 
     }
 }
