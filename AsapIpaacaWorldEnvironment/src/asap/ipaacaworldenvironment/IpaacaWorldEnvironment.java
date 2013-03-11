31ceca5e2efcf2c9a6947423425eb40aad73e14f
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-03-11 15:03:55 +0100

    added ipaacaworldenvironment

---
+++ AsapIpaacaWorldEnvironment/src/asap/ipaacaworldenvironment/IpaacaWorldEnvironment.java
@@ -0,0 +1,126 @@
+package asap.ipaacaworldenvironment;
+
+import hmi.animation.VJoint;
+import hmi.util.ClockListener;
+import hmi.worldobjectenvironment.WorldObject;
+import hmi.worldobjectenvironment.WorldObjectManager;
+import ipaaca.AbstractIU;
+import ipaaca.HandlerFunctor;
+import ipaaca.IUEventHandler;
+import ipaaca.IUEventType;
+import ipaaca.Initializer;
+import ipaaca.InputBuffer;
+import ipaaca.LocalMessageIU;
+import ipaaca.OutputBuffer;
+
+import java.util.EnumSet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.primitives.Floats;
+
+/**
+ * Manages world objects that are sent from an external rendering environment through ipaaca.
+ * @author hvanwelbergen
+ * 
+ */
+@Slf4j
+public class IpaacaWorldEnvironment implements ClockListener
+{
+    private final WorldObjectManager woManager;
+    private final OutputBuffer outBuffer;
+    private final InputBuffer inBuffer;
+
+    private final String SCENEINFO_CAT = "sceneinfo";
+    private final String COMMAND_KEY = "cmd";
+    private final String DATA_KEY = "data";
+    private final String REPORT_CMD = "report";
+    private final String REPORTING_CMD = "reporting";
+
+    static
+    {
+        Initializer.initializeIpaacaRsb();
+    }
+
+    public IpaacaWorldEnvironment(WorldObjectManager wm)
+    {
+        woManager = wm;
+        outBuffer = new OutputBuffer("IpaacaWorldEnvironment");
+        inBuffer = new InputBuffer("IpaacaWorldEnvironment", ImmutableSet.of(SCENEINFO_CAT));
+        inBuffer.registerHandler(new IUEventHandler(new SceneInfoHandler(), EnumSet.of(IUEventType.ADDED, IUEventType.UPDATED),
+                ImmutableSet.of(SCENEINFO_CAT)));
+    }
+
+    private void getReport()
+    {
+        LocalMessageIU message = new LocalMessageIU();
+        message.setCategory(SCENEINFO_CAT);
+        message.getPayload().put(COMMAND_KEY, REPORT_CMD);
+        outBuffer.add(message);
+    }
+
+    private void setWorldObject(String name, float[] pos)
+    {
+        WorldObject wo = woManager.getWorldObject(name);
+        VJoint vj = null;
+        if (wo != null)
+        {
+            vj = wo.getJoint();
+        }
+        else
+        {
+            vj = new VJoint(name, name);
+            wo = new WorldObject(vj);
+            woManager.addWorldObject(name, wo);
+        }
+        vj.setTranslation(pos);
+    }
+
+    class SceneInfoHandler implements HandlerFunctor
+    {
+        @Override
+        public void handle(AbstractIU iu, IUEventType type, boolean local)
+        {
+            if (iu.getPayload().get(COMMAND_KEY).equals(REPORTING_CMD))
+            {
+                String data = iu.getPayload().get(DATA_KEY);                
+                if (data.length() > 2)
+                {
+                    String elems[] = Iterables.toArray(
+                            Splitter.on(")(").trimResults().omitEmptyStrings().split(data.substring(1, data.length() - 1)), String.class);
+                    for (String elem:elems)
+                    {
+                        String s[]=elem.split("\\s+");
+                        if(s.length>=4)
+                        {
+                            String name = elem.substring(0,elem.indexOf(s[s.length-3])).trim();
+                            float x = Floats.tryParse(s[s.length-3]);
+                            float y = Floats.tryParse(s[s.length-2]);
+                            float z = Floats.tryParse(s[s.length-1]);
+                            setWorldObject(name, new float[]{x,y,z});
+                        }
+                    }
+                }
+            }
+            else
+            {
+                log.warn("Invalid command {}.", iu.getPayload().get(COMMAND_KEY));
+            }
+        }
+    }
+
+    @Override
+    public void initTime(double initTime)
+    {
+        getReport();
+    }
+
+    @Override
+    public void time(double currentTime)
+    {
+        getReport();
+    }
+}
