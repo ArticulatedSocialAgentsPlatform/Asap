34a6ddb39b101e31fe21b37fabaa81f4bd5fafde
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-05-10 14:07:27 +0200

    added textbehaviour

--- AsapBML/test/src/asap/bml/ext/bmlt/BMLTAudioFileBehaviourTest.java
+++ AsapBML/test/src/asap/bml/ext/bmlt/BMLTAudioFileBehaviourTest.java
@@ -1,4 +1,5 @@
 package asap.bml.ext.bmlt;
+
 import static org.junit.Assert.assertEquals;
 import hmi.xml.XMLFormatting;
 import hmi.xml.XMLTokenizer;
@@ -14,7 +15,7 @@ import saiba.utils.TestUtil;
 /**
  * Unit testcases for the BMLTAudioFileBehaviour
  * @author hvanwelbergen
- *
+ * 
  */
 public class BMLTAudioFileBehaviourTest extends AbstractBehaviourTest
 {
@@ -22,9 +23,9 @@ public class BMLTAudioFileBehaviourTest extends AbstractBehaviourTest
     @Override
     protected Behaviour createBehaviour(String bmlId, String extraAttributeString) throws IOException
     {
-        String str = "<bmlt:audiofile xmlns:bmlt=\"http://hmi.ewi.utwente.nl/bmlt\" "+TestUtil.getDefNS()+"id=\"a1\" fileName=\"blah.wav\" "+ 
-                extraAttributeString+"/>";        
-                return new BMLTAudioFileBehaviour(bmlId, new XMLTokenizer(str));
+        String str = "<bmlt:audiofile xmlns:bmlt=\"http://hmi.ewi.utwente.nl/bmlt\" " + TestUtil.getDefNS()
+                + "id=\"a1\" fileName=\"blah.wav\" " + extraAttributeString + "/>";
+        return new BMLTAudioFileBehaviour(bmlId, new XMLTokenizer(str));
     }
 
     @Override
@@ -32,32 +33,32 @@ public class BMLTAudioFileBehaviourTest extends AbstractBehaviourTest
     {
         return new BMLTAudioFileBehaviour(bmlId, new XMLTokenizer(bmlString));
     }
-    
+
     @Test
     public void testReadXML() throws IOException
     {
         String bmlString = "<bmlt:audiofile xmlns:bmlt=\"http://hmi.ewi.utwente.nl/bmlt\" id=\"a1\" start=\"nod1:end\" fileName=\"blah.wav\"/>";
         BMLTAudioFileBehaviour beh = new BMLTAudioFileBehaviour("bmla", new XMLTokenizer(bmlString));
-        assertEquals("bmla",beh.getBmlId());
-        assertEquals("a1",beh.id);
-        assertEquals("blah.wav",beh.getStringParameterValue("fileName"));
-        assertEquals("nod1",beh.getSyncPoints().get(0).getRef().sourceId);
-        assertEquals("end",beh.getSyncPoints().get(0).getRef().syncId);
+        assertEquals("bmla", beh.getBmlId());
+        assertEquals("a1", beh.id);
+        assertEquals("blah.wav", beh.getStringParameterValue("fileName"));
+        assertEquals("nod1", beh.getSyncPoints().get(0).getRef().sourceId);
+        assertEquals("end", beh.getSyncPoints().get(0).getRef().syncId);
     }
-    
+
     @Test
     public void writeXML() throws IOException
     {
         String bmlString = "<bmlt:audiofile xmlns:bmlt=\"http://hmi.ewi.utwente.nl/bmlt\" id=\"a1\" start=\"nod1:end\" fileName=\"blah.wav\"/>";
         BMLTAudioFileBehaviour behIn = new BMLTAudioFileBehaviour("bmla", new XMLTokenizer(bmlString));
-        StringBuilder buf = new StringBuilder();        
+        StringBuilder buf = new StringBuilder();
         behIn.appendXML(buf, new XMLFormatting(), "bmlt", "http://hmi.ewi.utwente.nl/bmlt");
         BMLTAudioFileBehaviour behOut = new BMLTAudioFileBehaviour("bmla", new XMLTokenizer(buf.toString()));
-        
-        assertEquals("bmla",behOut.getBmlId());
-        assertEquals("a1",behOut.id);
-        assertEquals("blah.wav",behOut.getStringParameterValue("fileName"));
-        assertEquals("nod1",behOut.getSyncPoints().get(0).getRef().sourceId);
-        assertEquals("end",behOut.getSyncPoints().get(0).getRef().syncId);
+
+        assertEquals("bmla", behOut.getBmlId());
+        assertEquals("a1", behOut.id);
+        assertEquals("blah.wav", behOut.getStringParameterValue("fileName"));
+        assertEquals("nod1", behOut.getSyncPoints().get(0).getRef().sourceId);
+        assertEquals("end", behOut.getSyncPoints().get(0).getRef().syncId);
     }
 }
