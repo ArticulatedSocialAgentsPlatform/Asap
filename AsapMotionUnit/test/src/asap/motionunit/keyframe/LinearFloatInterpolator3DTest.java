cc7362673e79910b1db9fa3968c9834e7ce62770
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-08-16 11:34:55 +0200

    test case for 2DOF LinearFloatInterpolator

--- AsapMotionUnit/test/src/asap/motionunit/keyframe/LinearFloatInterpolator3DTest.java
+++ AsapMotionUnit/test/src/asap/motionunit/keyframe/LinearFloatInterpolator3DTest.java
@@ -16,10 +16,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * @author hvanwelbergen
  *
  */
-public class LinearFloatInterpolatorTest
+public class LinearFloatInterpolator3DTest
 {
-    private LinearFloatInterpolator interp = new LinearFloatInterpolator();
+    private LinearFloatInterpolator interp3d = new LinearFloatInterpolator();
     private static final double TIME_PRECISION = 0.0001;
+    
     @Before
     public void setup()
     {
@@ -27,13 +28,13 @@ public class LinearFloatInterpolatorTest
         KeyFrame kf1 = new KeyFrame(0.2,new float[]{2,3,4});
         KeyFrame kf2 = new KeyFrame(0.8,new float[]{3,4,5});
         KeyFrame kf3 = new KeyFrame(1,new float[]{4,5,6});
-        interp.setKeyFrames(ImmutableList.of(kf0,kf1,kf2,kf3), 3);        
+        interp3d.setKeyFrames(ImmutableList.of(kf0,kf1,kf2,kf3), 3);
     }
     
     @Test
     public void testInterpolateToStart()
     {
-        KeyFrame kf = interp.interpolate(0);
+        KeyFrame kf = interp3d.interpolate(0);
         assertEquals(0, kf.getFrameTime(),TIME_PRECISION);
         assertThat(Floats.asList(kf.getDofs()), contains(1f,2f,3f));
     }
@@ -41,7 +42,7 @@ public class LinearFloatInterpolatorTest
     @Test
     public void testInterpolateToEnd()
     {
-        KeyFrame kf = interp.interpolate(1);
+        KeyFrame kf = interp3d.interpolate(1);
         assertEquals(1, kf.getFrameTime(),TIME_PRECISION);
         assertThat(Floats.asList(kf.getDofs()), contains(4f,5f,6f));
     }
@@ -49,7 +50,7 @@ public class LinearFloatInterpolatorTest
     @Test
     public void testInterpolate02()
     {
-        KeyFrame kf = interp.interpolate(0.2);
+        KeyFrame kf = interp3d.interpolate(0.2);
         assertEquals(0.2, kf.getFrameTime(),TIME_PRECISION);
         assertThat(Floats.asList(kf.getDofs()), contains(2f,3f,4f));
     }
@@ -57,7 +58,7 @@ public class LinearFloatInterpolatorTest
     @Test
     public void testInterpolate01()
     {
-        KeyFrame kf = interp.interpolate(0.1);
+        KeyFrame kf = interp3d.interpolate(0.1);
         assertEquals(0.1, kf.getFrameTime(),TIME_PRECISION);
         assertThat(Floats.asList(kf.getDofs()), contains(1.5f,2.5f,3.5f));
     }
