06f0f8f114dd089d941879851457e0ce8aac7467
Herwin van Welbergen <hvanwelbergen@TechFak.Uni-Bielefeld.DE>
2013-08-23 17:03:19 +0200

    better error messages

--- AsapIpaacaEmbodiments/test/src/asap/ipaacaembodiments/loader/IpaacaFaceAndBodyEmbodimentLoaderTest.java
+++ AsapIpaacaEmbodiments/test/src/asap/ipaacaembodiments/loader/IpaacaFaceAndBodyEmbodimentLoaderTest.java
@@ -9,10 +9,12 @@ import static org.mockito.Mockito.when;
 import hmi.animation.VJoint;
 import hmi.environmentbase.ClockDrivenCopyEnvironment;
 import hmi.environmentbase.Environment;
+import hmi.xml.XMLScanException;
 import hmi.xml.XMLTokenizer;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import asap.ipaacaembodiments.IpaacaEmbodiment;
@@ -30,17 +32,20 @@ public class IpaacaFaceAndBodyEmbodimentLoaderTest
     private IpaacaEmbodimentLoader mockEmbodimentLoader = mock(IpaacaEmbodimentLoader.class);
     private IpaacaEmbodiment mockEmbodiment = mock(IpaacaEmbodiment.class);
     
-    @SuppressWarnings("unchecked")
-    @Test
-    public void test() throws IOException
+    @Before
+    public void setup()
     {
         when(mockEmbodimentLoader.getEmbodiment()).thenReturn(mockEmbodiment);
         when(mockEmbodiment.getRootJointCopy(anyString())).thenReturn(new VJoint("BipKevin","BipKevin"));
         when(mockEmbodiment.getAvailableJoints()).thenReturn(ImmutableList.of("BipKevin"));
-               
-        
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void test() throws IOException
+    {
         String str = "<Loader id=\"ipaacabodyandfaceembodiment\" loader=\"asap.ipaacaembodiments.loader.IpaacaFaceAndBodyEmbodimentLoader\">"+
-        "<renaming skeletonRenamingFile=\"billieskeletonrenaming.xml\" morphsRenamingFile=\"billiemorphsrenaming.xml\"/>"+
+        "<renaming skeletonRenamingFile=\"billieskeletonrenaming.xml\" morphRenamingFile=\"billiemorphsrenaming.xml\"/>"+
         "</Loader>";
         ClockDrivenCopyEnvironment env = new ClockDrivenCopyEnvironment(20);
         IpaacaFaceAndBodyEmbodimentLoader loader = new IpaacaFaceAndBodyEmbodimentLoader();
@@ -50,5 +55,31 @@ public class IpaacaFaceAndBodyEmbodimentLoaderTest
         assertNotNull(loader.getEmbodiment());
         env.time(0);
         verify(mockEmbodiment).setJointData(any(ImmutableList.class), any(ImmutableMap.class));
+    }
+    
+    @Test(expected=XMLScanException.class)
+    public void testNonExistingSkeletonRenamingFile() throws IOException
+    {
+        String str = "<Loader id=\"ipaacabodyandfaceembodiment\" loader=\"asap.ipaacaembodiments.loader.IpaacaFaceAndBodyEmbodimentLoader\">"+
+        "<renaming skeletonRenamingFile=\"nonexistantskel.xml\"/>"+
+        "</Loader>";
+        ClockDrivenCopyEnvironment env = new ClockDrivenCopyEnvironment(20);
+        IpaacaFaceAndBodyEmbodimentLoader loader = new IpaacaFaceAndBodyEmbodimentLoader();
+        XMLTokenizer tok = new XMLTokenizer(str);
+        tok.takeSTag();
+        loader.readXML(tok, "id1", "id1", "id1", ImmutableList.of(env).toArray(new Environment[0]), mockEmbodimentLoader);        
+    }
+    
+    @Test(expected=XMLScanException.class)
+    public void testNonExistingMorphRenamingFile() throws IOException
+    {
+        String str = "<Loader id=\"ipaacabodyandfaceembodiment\" loader=\"asap.ipaacaembodiments.loader.IpaacaFaceAndBodyEmbodimentLoader\">"+
+        "<renaming skeletonRenamingFile=\"billieskeletonrenaming.xml\" morphRenamingFile=\"nonexistantskel.xml\"/>"+
+        "</Loader>";
+        ClockDrivenCopyEnvironment env = new ClockDrivenCopyEnvironment(20);
+        IpaacaFaceAndBodyEmbodimentLoader loader = new IpaacaFaceAndBodyEmbodimentLoader();
+        XMLTokenizer tok = new XMLTokenizer(str);
+        tok.takeSTag();
+        loader.readXML(tok, "id1", "id1", "id1", ImmutableList.of(env).toArray(new Environment[0]), mockEmbodimentLoader);        
     }
 }
