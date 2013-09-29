c5709c31e3b155cc063608066c3898c09e2fa86c
hvanwelbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2013-09-29 16:35:39 +0200

    graceful interrupt test

--- AsapAnimationEngine/test/src/asap/animationengine/pointing/PointingTMUTest.java
+++ AsapAnimationEngine/test/src/asap/animationengine/pointing/PointingTMUTest.java
@@ -1,5 +1,6 @@
 package asap.animationengine.pointing;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -13,6 +14,7 @@ import hmi.worldobjectenvironment.WorldObjectManager;
 
 import java.util.Set;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -28,7 +30,8 @@ import asap.realizer.pegboard.BMLBlockPeg;
 import asap.realizer.pegboard.PegBoard;
 import asap.realizer.pegboard.TimePeg;
 import asap.realizer.planunit.KeyPosition;
-import asap.realizer.planunit.TimedPlanUnit;
+import asap.realizer.planunit.TimedPlanUnitPlayException;
+import asap.realizer.planunit.TimedPlanUnitState;
 import asap.realizer.scheduler.BMLBlockManager;
 import asap.realizertestutil.planunit.AbstractTimedPlanUnitTest;
 import asap.realizertestutil.util.TimePegUtil;
@@ -46,10 +49,11 @@ public class PointingTMUTest extends AbstractTimedPlanUnitTest
     private AnimationPlayer mockAnimationPlayer = mock(AnimationPlayer.class);
     private PegBoard pegBoard = new PegBoard();
     private TimedAnimationMotionUnit mockRelaxTMU = mock(TimedAnimationMotionUnit.class);
+    private static final double TIME_PRECISION = 0.001;
 
     @SuppressWarnings("unchecked")
     @Override
-    protected TimedPlanUnit setupPlanUnit(FeedbackManager bfm, BMLBlockPeg bbPeg, String id, String bmlId, double startTime)
+    protected PointingTMU setupPlanUnit(FeedbackManager bfm, BMLBlockPeg bbPeg, String id, String bmlId, double startTime)
     {
         VJoint vCurr = HanimBody.getLOA1HanimBody();
         VJoint vNext = HanimBody.getLOA1HanimBody();
@@ -72,12 +76,13 @@ public class PointingTMUTest extends AbstractTimedPlanUnitTest
         RestPose mockRestPose = mock(RestPose.class);
         AnimationUnit mockRelaxMU = mock(AnimationUnit.class);
         when(mockAnimationPlayer.getRestPose()).thenReturn(mockRestPose);
+        when(mockRestPose.getTransitionToRestDuration(any(VJoint.class), any(Set.class))).thenReturn(2d);
         when(mockRestPose.createTransitionToRest((Set<String>) any())).thenReturn(mockRelaxMU);
         when(
                 mockAnimationPlayer.createTransitionToRest(any(FeedbackManager.class), (Set<String>) any(), any(TimePeg.class),
                         any(TimePeg.class), any(String.class), any(String.class), any(BMLBlockPeg.class), any(PegBoard.class))).thenReturn(
                 mockRelaxTMU);
-
+        when(mockAnimationPlayer.getTransitionToRestDuration(any(Set.class))).thenReturn(2d);
         mu.addKeyPosition(new KeyPosition(BMLGestureSync.STROKE_START.getId(), 0.2, 1.0));
         mu.addKeyPosition(new KeyPosition(BMLGestureSync.STROKE_END.getId(), 0.8, 1.0));
 
@@ -89,4 +94,22 @@ public class PointingTMUTest extends AbstractTimedPlanUnitTest
         return tmu;
     }
 
+    @Test
+    public void testGracefullInterrupt() throws TimedPlanUnitPlayException
+    {
+        PointingTMU tmu = setupPlanUnit(fbManager, BMLBlockPeg.GLOBALPEG, "beh1", "bml1", 0);
+        tmu.setTimePeg("ready", TimePegUtil.createTimePeg(1));
+        tmu.setTimePeg("relax", TimePegUtil.createTimePeg(2));
+        tmu.setTimePeg("end", TimePegUtil.createTimePeg(3));
+        tmu.setState(TimedPlanUnitState.LURKING);
+        tmu.start(0);
+        tmu.play(0);        
+        assertEquals(TimedPlanUnitState.IN_EXEC, tmu.getState());
+        tmu.interrupt(0.5f);
+        tmu.play(0.49);
+        tmu.play(0.495);
+        assertEquals(TimedPlanUnitState.SUBSIDING, tmu.getState());
+        assertEquals(0.49,tmu.getRelaxTime(), TIME_PRECISION);
+        assertEquals(2.49,tmu.getEndTime(), TIME_PRECISION);
+    }
 }
