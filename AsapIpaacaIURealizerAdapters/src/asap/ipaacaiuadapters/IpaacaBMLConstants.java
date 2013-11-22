335f0cb8b6be0bf99933634821a19cd1e0ccbeae
Ramin Yaghoubzadeh <ryaghoubzadeh@uni-bielefeld.de>
2013-11-22 16:43:07 +0100

    added 1st version of new ipaaca IU-integrated feedback
    Global times are still off. Will probably solve using a handshake.

---
+++ AsapIpaacaIURealizerAdapters/src/asap/ipaacaiuadapters/IpaacaBMLConstants.java
@@ -0,0 +1,20 @@
+package asap.ipaacaiuadapters;
+
+/**
+ * Constants for categories and payload keys
+ * @author Herwin
+ *
+ */
+public final class IpaacaBMLConstants
+{
+    private IpaacaBMLConstants(){}
+    
+    public static final String BML_CATEGORY = "bml";
+    public static final String BML_FEEDBACK_CATEGORY = "bmlfeedback";
+    public static final String BML_FEEDBACK_KEY = "bmlfeedback";
+    public static final String BML_KEY = "bml";
+    public static final String BML_ID_KEY = "bmlid";
+    public static final String IU_STATUS_KEY = "status";
+    public static final String IU_PREDICTED_END_TIME_KEY = "predictedEndTime";
+    public static final String IU_PREDICTED_START_TIME_KEY = "predictedStartTime";
+}
---
+++ AsapIpaacaIURealizerAdapters/src/asap/ipaacaiuadapters/IpaacaToBMLRealizerAdapter.java
@@ -0,0 +1,182 @@
+package asap.ipaacaiuadapters;
+
+import ipaaca.AbstractIU;
+import ipaaca.HandlerFunctor;
+import ipaaca.IUEventHandler;
+import ipaaca.IUEventType;
+import ipaaca.Initializer;
+import ipaaca.InputBuffer;
+import ipaaca.OutputBuffer;
+import ipaaca.util.ComponentNotifier;
+
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.HashMap;
+
+import saiba.bml.core.BehaviourBlock;
+import saiba.bml.feedback.BMLBlockPredictionFeedback;
+import saiba.bml.feedback.BMLBlockProgressFeedback;
+import saiba.bml.feedback.BMLFeedback;
+import saiba.bml.feedback.BMLFeedbackParser;
+import saiba.bml.feedback.BMLPredictionFeedback;
+import asap.realizerport.BMLFeedbackListener;
+import asap.realizerport.RealizerPort;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Submits ipaaca messages (from an InputBuffer) to a RealizerPort; submits RealizerPort feedbacks to the OutputBuffer. 
+ * Assumes that the connected realizerport is threadsafe (or at least that its performBML function is).
+ * @author Herwin
+ */
+public class IpaacaToBMLRealizerAdapter implements BMLFeedbackListener
+{
+    static double initTime =  System.currentTimeMillis() / 1000.0;
+    static
+    {
+        //final double initTime =  System.nanoTime() / 1000000000L;
+        Initializer.initializeIpaacaRsb();
+    }
+    
+
+    //private double initTime;
+    private final InputBuffer inBuffer = new InputBuffer("IpaacaToBMLRealizerAdapter", ImmutableSet.of(IpaacaBMLConstants.BML_CATEGORY));
+    private final OutputBuffer outBuffer = new OutputBuffer("IpaacaToBMLRealizerAdapter");
+    private final RealizerPort realizerPort;
+    private long nextUsedAutoBmlId = 1;
+    private HashMap<String, String> bmlIdToIuIdMap;
+    private HashMap<String, String> iuIdToBmlIdMap;
+    
+    public synchronized String autoGenerateBmlId(String prefix)
+    {
+    	return prefix+"_"+String.valueOf(nextUsedAutoBmlId++);
+    }
+    
+    public IpaacaToBMLRealizerAdapter(RealizerPort port)
+    {
+    	this.bmlIdToIuIdMap = new HashMap<String, String>();
+    	this.iuIdToBmlIdMap = new HashMap<String, String>();
+        this.realizerPort = port;        
+        realizerPort.addListeners(this);
+        EnumSet<IUEventType> types = EnumSet.of(IUEventType.ADDED);
+        inBuffer.registerHandler(new IUEventHandler(new HandlerFunctor()
+        {
+            @Override
+            public void handle(AbstractIU iu, IUEventType type, boolean local)
+            {
+            	BehaviourBlock bb = new BehaviourBlock();
+            	bb.readXML(iu.getPayload().get(IpaacaBMLConstants.BML_KEY));
+            	String actualBmlId = bb.getBmlId();
+            	if (actualBmlId.equals("AUTO")) {
+            		String[] path_elements = iu.getOwnerName().split("/");
+            		String prefix = "autogen";
+            		if (path_elements.length > 1) {
+            			prefix = path_elements[path_elements.length - 2];
+            		}
+            		bb.setBmlId(autoGenerateBmlId(prefix));
+            		actualBmlId = bb.getBmlId();
+            	}
+            	bmlIdToIuIdMap.put(actualBmlId, iu.getUid());
+            	iuIdToBmlIdMap.put(iu.getUid(), actualBmlId);
+            	String actualBmlString = bb.toXMLString();
+                realizerPort.performBML(actualBmlString);
+                HashMap<String, String> items = new HashMap<String, String>();
+                items.put(IpaacaBMLConstants.BML_KEY, actualBmlString);
+                items.put(IpaacaBMLConstants.BML_ID_KEY, actualBmlId);
+                items.put(IpaacaBMLConstants.IU_STATUS_KEY, "((RECEIVED))");
+            	try{
+            		iu.getPayload().merge(items);
+            	} catch (ipaaca.IUUpdateFailedException ex) {
+            		//
+            	}
+           	}
+        }, types, ImmutableSet.of(IpaacaBMLConstants.BML_CATEGORY)));
+        
+        ComponentNotifier notifier = new ComponentNotifier("IpaacaToBMLRealizerAdapter", "bmlrealizer",
+                ImmutableSet.of(IpaacaBMLConstants.BML_FEEDBACK_CATEGORY),ImmutableSet.of(IpaacaBMLConstants.BML_CATEGORY),
+                outBuffer, inBuffer);
+        notifier.initialize();
+    }
+
+    @Override
+    public void feedback(String feedback)
+    {
+        HashMap<String, String> new_payload_items = new HashMap<String, String>();    	
+        /*LocalMessageIU feedbackIU = new LocalMessageIU();
+        feedbackIU.setCategory(IpaacaBMLConstants.BML_FEEDBACK_CATEGORY);
+        feedbackIU.getPayload().put(IpaacaBMLConstants.BML_FEEDBACK_KEY, feedback);
+        outBuffer.add(feedbackIU);
+        */
+        BMLFeedback fb;
+        try
+        {
+            fb = BMLFeedbackParser.parseFeedback(feedback);
+        }
+        catch (IOException e)
+        {
+            // shouldn't happen since we parse strings
+            throw new AssertionError(e);
+        }
+        if (fb instanceof BMLBlockProgressFeedback)
+        {
+            AbstractIU iu_to_update = null;
+            BMLBlockProgressFeedback fbBlock = (BMLBlockProgressFeedback) fb;
+            if (bmlIdToIuIdMap.containsKey(fbBlock.getBmlId())) {
+            	iu_to_update = inBuffer.getIU(bmlIdToIuIdMap.get(fbBlock.getBmlId()));
+            	if (fbBlock.getSyncId().equals("end"))
+                {
+                	new_payload_items.put(IpaacaBMLConstants.IU_STATUS_KEY, "DONE");
+                }
+                else if (fbBlock.getSyncId().equals("start"))
+                {
+                	new_payload_items.put(IpaacaBMLConstants.IU_STATUS_KEY, "IN_EXEC");
+                }
+            } else {
+            	// ignore this BML block: it was not added by us
+            }
+            if (iu_to_update != null) {
+            	try{
+            		iu_to_update.getPayload().merge(new_payload_items);
+            	} catch (ipaaca.IUUpdateFailedException ex) {
+            		//
+            	}
+            }
+        }
+        else if (fb instanceof BMLPredictionFeedback)
+        {
+            BMLPredictionFeedback pf = (BMLPredictionFeedback) fb;
+        	// FIXME: no clue how to obtain the correct bml ID here...
+            //String bmlid = pf.getBmlBehaviorPredictions().get(0).getBmlId();
+            //iu_to_update = inBuffer.getIU(bmlIdToIuIdMap.get(bmlid));
+            double latestPredictedEndTime = -1;
+            double earliestPredictedStartTime = 9999999999999999999999999.9;
+            for (BMLBlockPredictionFeedback bbp : pf.getBmlBlockPredictions())
+            {
+                AbstractIU iu_to_update = inBuffer.getIU(bmlIdToIuIdMap.get(bbp.getId()));            	
+            	//System.out.println("id="+id);
+            	double start = bbp.getGlobalStart() + initTime;
+            	double end = bbp.getGlobalEnd() + initTime;
+            	if (end>latestPredictedEndTime) { latestPredictedEndTime = end; }
+            	if (start<earliestPredictedStartTime) { earliestPredictedStartTime = start; }
+            	new_payload_items.put(IpaacaBMLConstants.IU_PREDICTED_START_TIME_KEY, String.format("%.3f", start)); 
+            	new_payload_items.put(IpaacaBMLConstants.IU_PREDICTED_END_TIME_KEY, String.format("%.3f", end));
+            	double t = System.currentTimeMillis()/1000.0;
+            	System.out.println("SEND TIME: "+String.format("%.3f", t));
+                if (iu_to_update != null) {
+                	try{
+                		iu_to_update.getPayload().merge(new_payload_items);
+                	} catch (ipaaca.IUUpdateFailedException ex) {
+                		//
+                	}
+                }
+            }
+            // TODO: also send a global IU/Message with the latest known end time?
+        }
+    }
+
+    public void close()
+    {
+        outBuffer.close();
+        inBuffer.close();
+    }
+}
