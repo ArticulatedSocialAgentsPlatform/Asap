41d26d960516f389ff1674c8d6cd092a21b3aa61
Herwin van Welbergen <hvanwelbergen@techfak.uni-bielefeld.de>
2014-02-28 21:07:14 +0100

    x

--- AsapRsbEmbodiments/java/src/asap/rsbembodiments/RsbRpcReceive.java
+++ AsapRsbEmbodiments/java/src/asap/rsbembodiments/RsbRpcReceive.java
@@ -3,8 +3,13 @@ package asap.rsbembodiments;
 import rsb.Event;
 import rsb.Factory;
 import rsb.RSBException;
+import rsb.converter.DefaultConverterRepository;
+import rsb.converter.ProtocolBufferConverter;
 import rsb.patterns.EventCallback;
 import rsb.patterns.LocalServer;
+import asap.rsbembodiments.Rsbembodiments.Joint;
+import asap.rsbembodiments.Rsbembodiments.JointDataConfigReply;
+import asap.rsbembodiments.Rsbembodiments.JointDataConfigRequest;
 
 public class RsbRpcReceive
 {
@@ -13,18 +18,28 @@ public class RsbRpcReceive
         @Override
         public Event invoke(final Event request) throws Throwable
         {
-            return new Event(String.class, request.getData());
+            System.out.println("invoke");
+            JointDataConfigRequest jdcr = (JointDataConfigRequest)request.getData();
+            Joint j = Joint.newBuilder().setId(jdcr.getId()+"-HumanoidRood").setParentId("parent").build();
+            return new Event(JointDataConfigReply.class, JointDataConfigReply.newBuilder().addJoint(j).build());            
         }
 
     }
 
     public static void main(String args[]) throws RSBException
     {
+        final ProtocolBufferConverter<JointDataConfigRequest> converter = new ProtocolBufferConverter<JointDataConfigRequest>(
+                JointDataConfigRequest.getDefaultInstance());
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(converter);
+        final ProtocolBufferConverter<JointDataConfigReply> converter2 = new ProtocolBufferConverter<JointDataConfigReply>(
+                JointDataConfigReply.getDefaultInstance());
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(converter2);
+        
         final LocalServer server = Factory.getInstance().createLocalServer("/example/server");
         server.activate();
 
         // Add method an "echo" method, implemented by EchoCallback.
-        server.addMethod("echo", new EchoCallback());
+        server.addMethod("jointDataConfigRequest", new EchoCallback());
         
         // Block until server.deactivate or process shutdown
         server.waitForShutdown();
--- AsapRsbEmbodiments/java/src/asap/rsbembodiments/RsbRpcSend.java
+++ AsapRsbEmbodiments/java/src/asap/rsbembodiments/RsbRpcSend.java
@@ -1,12 +1,23 @@
 package asap.rsbembodiments;
 
 import rsb.Factory;
+import rsb.converter.DefaultConverterRepository;
+import rsb.converter.ProtocolBufferConverter;
 import rsb.patterns.RemoteServer;
+import asap.rsbembodiments.Rsbembodiments.JointDataConfigReply;
+import asap.rsbembodiments.Rsbembodiments.JointDataConfigRequest;
 
 public class RsbRpcSend
 {
     public static void main(final String[] args) throws Throwable
     {
+        final ProtocolBufferConverter<JointDataConfigRequest> converter = new ProtocolBufferConverter<JointDataConfigRequest>(
+                JointDataConfigRequest.getDefaultInstance());
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(converter);
+        final ProtocolBufferConverter<JointDataConfigReply> converter2 = new ProtocolBufferConverter<JointDataConfigReply>(
+                JointDataConfigReply.getDefaultInstance());
+        DefaultConverterRepository.getDefaultConverterRepository().addConverter(converter2);
+        
         // Get remote server object to call exposed request methods of
         // participants
         final RemoteServer server = Factory.getInstance().createRemoteServer("/example/server");
@@ -15,7 +26,10 @@ public class RsbRpcSend
         // Call remote method and deactivate the server.
         try
         {
-            System.out.println("Server replied: " + server.call("echo", 1d));
+            Rsbembodiments.JointDataConfigReply reply = server.call("jointDataConfigRequest", Rsbembodiments.JointDataConfigRequest
+                    .newBuilder().setId("billie").build());
+
+            System.out.println("Server replied: " + reply.getJointList().get(0).getId());
         }
         finally
         {
